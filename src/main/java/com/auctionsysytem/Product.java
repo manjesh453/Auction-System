d78cec88f2481f8abdb92ec43d3708970c0531e7
manjesh453 <rayamajhimanjesh41@gmail.com>
2024-11-26 17:40:57 +0545

    Creating Product class

---
+++ src/main/java/com/auctionsysytem/Product.java
@@ -0,0 +1,18 @@
+package com.auctionsysytem;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer productId;
+    private String productName;
+    private String productDescription;
+    private String productPrice;
+}
